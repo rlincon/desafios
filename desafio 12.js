6133b4afc5ef23e13ca0eaedde8d9179e1f6d7f4
rlincon <82413456+rlincon@users.noreply.github.com>
2024-08-07 10:49:02 -0300

    Add files via upload

---
+++ desafio 12.js
@@ -0,0 +1,13 @@
+var jogahtml = window.document.getElementById('saida')
+
+function clique1() {
+    jogahtml.innerHTML += '<p> Você Clicou escolheu a primeira opção = botão 1 </p>'
+}
+
+function clique2() {
+    jogahtml.innerHTML += '<p> Você Clicou escolheu a primeira opção = botão 2 </p>'
+}
+
+function clique3() {
+    jogahtml.innerHTML += '<p> Você Clicou escolheu a primeira opção = botão 3</p>'
+}
